7fef9a7d8e02a588ea268be0267153cfefda0927
kristensen-kor <kristensen666@gmail.com>
2025-06-13 16:12:08 +0300

    added prob_round()

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 // main.js
 
 import { createApp } from "vue";
-import { sum, lr_round, to_matrix} from "./utils.js";
+import { sum, to_matrix, lr_round, prob_round } from "./utils.js";
+
+const algorithm_map = {
+	lr_round: lr_round,
+	prob_round: prob_round
+};
 
 const app_config = {
 	data() {
 		return {
 			input_text: "",
+			algorithm: "lr_round",
 			sum: null
 		};
 	},
+	computed: {
+		rounding_function() {
+			return algorithm_map[this.algorithm];
+		}
+	},
 	methods: {
 		read_round_copy() {
-			const matrix = this.input_text.replaceAll.split("\n").map(row => row.split("\t").map(Number));
-			const rounded_matrix = to_matrix(lr_round(matrix.flat()), matrix.length);
+			const matrix = this.input_text.replaceAll(",", ".").split("\n").map(row => row.split("\t").map(Number));
+			const rounded_matrix = to_matrix(this.rounding_function(matrix.flat()), matrix.length);
 
 			this.sum = sum(rounded_matrix.map(sum));
 			this.input_text = rounded_matrix.map(row => row.join("\t")).join("\n");
--- utils.js
+++ utils.js
@@ -1,5 +1,7 @@
 // utils.js
 
+const range = n => Array.from({ length: n }, (_, i) => i);
+
 export const sum = xs => xs.reduce((a, b) => a + b, 0);
 
 export function to_matrix(xs, rows) {
@@ -9,9 +11,7 @@ export function to_matrix(xs, rows) {
 
 export function lr_round(xs) {
 	let base_array = xs.map(Math.floor);
-
 	const difference = Math.round(sum(xs)) - sum(base_array);
-
 	const remainders = xs.map((num, index) => [num - Math.floor(num), index]).toSorted((a, b) => b[0] - a[0]);
 
 	for (let i = 0; i < difference; i++) {
@@ -19,4 +19,25 @@ export function lr_round(xs) {
 	}
 
 	return base_array;
+}
+
+export function prob_round(xs) {
+	let rounded_array = xs.map(Math.floor);
+	const diff = Math.round(sum(xs)) - sum(rounded_array);
+
+	if (diff > 0) {
+		const remainders = xs.map((x, i) => x - rounded_array[i]);
+		const sampled_indices = weighted_sample(xs.length, remainders, diff);
+
+		for (const idx of sampled_indices) {
+			rounded_array[idx]++;
+		}
+	}
+
+	return rounded_array;
+}
+
+function weighted_sample(n, weights, k) {
+	const keys = range(n).map((x, i) => ({ x, key: Math.random() ** (1 / weights[i]) }));
+	return keys.toSorted((a, b) => b.key - a.key).slice(0, k).map(e => e.x);
 }
